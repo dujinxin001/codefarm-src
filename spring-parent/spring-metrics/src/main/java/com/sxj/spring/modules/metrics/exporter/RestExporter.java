f0230524b35666af4632f841e32369677ae8103b
dujinxin <dujinxin@windows10.microdone.cn>
2018-06-05 11:54:06 +0800

    update Change-Id: Ib6f08a8d8b609310cc76113b6f0be7e9ad35c4c1

--- spring-parent/spring-metrics/src/main/java/com/sxj/spring/modules/metrics/exporter/RestExporter.java
+++
@@ -1,8 +0,0 @@
-package com.sxj.spring.modules.metrics.exporter;
-
-/**
- * Export as Restful Service in json format.
- */
-public class RestExporter {
-
-}
--- spring-parent/spring-util/src/main/java/com/codefarm/spring/modules/util/serializer/Serializer.java
+++
@@ -1,8 +0,0 @@
-package com.codefarm.spring.modules.util.serializer;
-
-public interface Serializer
-{
-    public String serialize(final Object obj);
-    
-    public Object deserialize(final String str);
-}
